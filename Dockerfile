8a61511bc0d9da4132bb466d16624caf9a17b629
hitaloalvess <hitalo.ralves@outlook.com>
2022-07-01 15:52:05 -0300

    :bricks: ci:  modify dockerfile and docker-compose

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ COPY . .
 
 EXPOSE 3333
 
+RUN npx prisma generate
+
 CMD ["npm", "run", "dev"]
--- src/shared/infra/http/server.ts
+++ src/shared/infra/http/server.ts
@@ -1,3 +1,5 @@
 import { app } from './app';
 
-app.listen(3333, () => console.log('Server listen, port 3333'));
+app.listen(process.env.PORT || 3333, () =>
+    console.log('Server listen, port 3333'),
+);
